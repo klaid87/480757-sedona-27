c9b818daac1f1539589cc33b452f122896a92ab3
klaid87 <37298521+klaid87@users.noreply.github.com>
2020-02-20 23:16:21 +0300

    Мелкие правки

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var button = document.querySelector(".search-modal-button");
+var popup = document.querySelector(".search-form");
+
+
+
+button.addEventListener("clic", function() {
+});
+popup.classList.add("form-show");
