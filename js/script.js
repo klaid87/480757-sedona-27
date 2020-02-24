df33281259386e2467a18edbbc0dff3e6c09d846
klaid87 <37298521+klaid87@users.noreply.github.com>
2020-02-24 16:20:30 +0300

    Редактирование кода, исправление ошибок

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 var button = document.querySelector(".search-modal-button");
 var popup = document.querySelector(".search-form");
+var checkIn = document.querySelector("[name=check-in]");
 
 
-
-button.addEventListener("clic", function(evt) {
+button.addEventListener("click", function(evt) {
+    evt.preventDefault();
     popup.classList.add("form-show");
 });
