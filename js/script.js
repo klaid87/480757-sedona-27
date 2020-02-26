d45cbe8c3a65c71027494f0f710eca409ec368fc
klaid87 <37298521+klaid87@users.noreply.github.com>
2020-02-26 16:14:26 +0300

    Мелкие правки в коде

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 var button = document.querySelector(".search-modal-button");
 var popup = document.querySelector(".search-form");
-var checkIn = document.querySelector("[name=check-in]");
+var checkIn = document.querySelector("[name=date-check-in]");
 
 
 button.addEventListener("click", function(evt) {
     evt.preventDefault();
     popup.classList.toggle("form-hide");
+    checkIn.focus();
 });
